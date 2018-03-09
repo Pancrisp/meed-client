2ed816ceced08de68e6cdad6c303ef855fe13e81
Ervin Chua <ervinchua@gmail.com>
2018-03-09 22:15:08 +1100

    Login, signup components

---
+++ ClientApp/src/views/Login.vue
@@ -0,0 +1,13 @@
+<template>
+  
+</template>
+
+<script>
+import Login from "@/components/Login.vue";
+export default {
+  name: "login",
+  components: {
+    Login
+  }
+}
+</script>
---
+++ ClientApp/src/views/SignUp.vue
@@ -0,0 +1,13 @@
+<template>
+  
+</template>
+
+<script>
+import SignUp from "@/components/SignUp.vue";
+export default {
+  name: "signup",
+  components: {
+    SignUp
+  }
+}
+</script>
