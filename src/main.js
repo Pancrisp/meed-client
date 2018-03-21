0bdad1beb69a49956ff44f14bfff529f7001f207
Diane Loi <dianeloi_8@hotmail.com>
2018-03-21 16:27:22 +1100

    Configure firebase to project

--- src/main.js
+++ src/main.js
@@ -1,7 +1,19 @@
 import Vue from "vue";
 import App from "./App.vue";
+import firebase from "firebase";
 import router from "./router";
 import store from "./store";
+
+// Initialize Firebase
+  const config = {
+    apiKey: "AIzaSyB8uW7ccg7vkqhtNSftBfimvgZNOP_ilcI",
+    authDomain: "meed-84026.firebaseapp.com",
+    databaseURL: "https://meed-84026.firebaseio.com",
+    projectId: "meed-84026",
+    storageBucket: "",
+    messagingSenderId: "1096524670287"
+  };
+  firebase.initializeApp(config);
 
 Vue.config.productionTip = false;
 
