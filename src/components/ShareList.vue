8120290c2802f4441806c8f3a7cd5a0cb066662b
rmit-s3484880-emily-yorgey <s3484880@student.rmit.edu.au>
2018-04-10 14:08:52 +1000

    push to array

---
+++ src/components/ShareList.vue
@@ -0,0 +1,56 @@
+<template>
+    <div class="share-list">
+        <ul>
+    <li v-for="share in shares">
+    {{shareListings}}
+    </li>
+</ul>
+<input
+    v-model="newShareListing"
+    @ 
+    placeholder = "List Shares"
+/>
+</template>
+
+<script>
+import ShareItem from "@/components/ShareItem.vue";
+
+
+export default {
+
+data: {
+newShareListing: '',
+    shareListings: []
+    symbol: "",
+    id: "",
+    name: "",
+    price: ""
+  },
+},
+methods: {
+    addShareListing() {
+        this.shareListings.push (this.newShareListing = '')
+        }
+    }
+]);
+
+
+
+//   created: {
+//     loadShares() {
+//       axios.get("http://localhost:5000/shares");
+
+//       // retrieve a list of shares from the API
+//       // loop through each share and save the fields to an object.
+//       // pass the object into the shares array using the push() method
+//       object.id = res.data[0];
+
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
+
+
+
