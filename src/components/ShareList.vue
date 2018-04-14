b3f9a1bb2070301d8df8467789ef37e04ee5b394
Ervin Chua <ervinchua@gmail.com>
2018-04-14 12:48:07 +1000

    Passing props from ShareList into ShareItem component
    - Refactor templates in both components to improve semantics and readability
    
    - Hard coded shares data for initial testing

--- src/components/ShareList.vue
+++ src/components/ShareList.vue
@@ -1,48 +1,54 @@
 <template>
   <div>
-    <ul>
-      <li v-for="share in shares" :key="share.symbol">
-        {{ share }}
-      </li>
-
+    <ul v-for="share in shares" :key="share.symbol">
+      <ShareItem :symbol="share.symbol" :name="share.name" :price="share.price" />
     </ul>
   </div>
 </template>
 
 <script>
 import axios from "axios";
+import ShareItem from "./ShareItem.vue";
 
 export default {
   name: "ShareList",
+  components: {
+    ShareItem
+  },
   data() {
     return {
       shares: [
         {
           symbol: "MSFT",
+          name: "Microsoft",
           price: "90.6900",
           volume: "31501471",
           timestamp: "2018-04-09 16:27:42"
         },
         {
           symbol: "FB",
+          name: "Facebook",
           price: "157.8500",
           volume: "34682258",
           timestamp: "2018-04-09 16:46:39"
         },
         {
           symbol: "AAPL",
+          name: "Apple",
           price: "170.1800",
           volume: "28971635",
           timestamp: "2018-04-09 16:14:50"
         },
         {
           symbol: "NVDA",
+          name: "NVIDIA",
           price: "217.4000",
           volume: "12429542",
           timestamp: "2018-04-09 17:00:00"
         },
         {
           symbol: "TSLA",
+          name: "TESLA",
           price: "289.6300",
           volume: "10241500",
           timestamp: "2018-04-09 16:00:00"
