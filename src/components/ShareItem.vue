dbd36b49765d9cf1b277846f092fc2f7e2ee0025
rmit-s3484880-emily-yorgey <s3484880@student.rmit.edu.au>
2018-04-15 11:39:24 +1000

    buy/sell button test

--- src/components/ShareItem.vue
+++ src/components/ShareItem.vue
@@ -1,7 +1,17 @@
 <template>
-  <div>
-    <li></li>
-  </div>
+  <tr>
+    <div class id = "app">
+      <td> {{symbol}} </td>
+      <td> {{name}} </td>
+      <td> {{price | currency}} </td>
+      <td> {{timestamp}} </td>
+      <div class= "buy-share" v-show="!formBuyOpen" @click="formOpen= true"> Buy
+        <form @submit = "cancel()">
+      <div class= "sell-share" v-show="!formSellOpen" @click="formOpen= true"> Sell
+        <form @submit = "cancel()">
+      </div>
+  </tr>
+
 </template>
 
 <script>
@@ -9,17 +19,36 @@ export default {
   name: ShareItem,
   data() {
     return {
+      formBuyOpen: false,
+      formSellOpen: false,
       symbol: "",
       name: "",
       price: "",
       timestamp: ""
     };
   },
+
   props: ["symbol", "name", "price", "timestamp"],
-  computed: {}
+  computed: {},
+  filters: {
+    currency(price) {
+      return price.toFixed(2);
+    }
+  }
 };
 </script>
 
 <style lang="scss" scoped>
+html,
+body {
+  height: 100%;
+  margin: 0;
+}
 
+body {
+  font-family: "Open Sans", sans-serif;
+  font-size: 16px;
+  background-color: #f3f4f5;
+  cursor: default;
+}
 </style>
