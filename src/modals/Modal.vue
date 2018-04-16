90bd7f8af20bc1e82d0d399311b9d7549fc0a1b6
rmit-s3484880-emily-yorgey <s3484880@student.rmit.edu.au>
2018-04-16 18:00:11 +1000

    finish modal component design

--- src/modals/Modal.vue
+++ src/modals/Modal.vue
@@ -3,34 +3,31 @@
     <div class="modal-mask">
       <div class="modal-wrapper">
         <div class="modal-container">
-          <form @submit.prevent="buy">
-            <div>
-              <label>Symbol</label>
-              <input type="text" :value="symbol" disabled>
-            </div>
-
-            <div>
-              <label>Company Name</label>
+          <form class="flex" @submit.prevent="buy">
+            <div class="quantity flex-2">
+              <div class="flex">
+              <label>Name</label>
               <input type="text" :value="name" disabled>
-            </div>
+              </div>
 
-            <div>
-              <label>Price</label>
-              <input type="text" :value="price" disabled>
-            </div>
+              <div class="flex">
+                <label>Price</label>
+                <input type="text" :value="price" disabled>
+              </div>
 
-            <div>
-              <label>Quantity</label>
-              <input type="text" v-model="quantity" required>
+              <div class="flex">
+                <label>Quantity</label>
+                <input type="text" v-model="quantity" @keyup="calcCost" required>
+              </div>
             </div>
-
-            <div v-if="quantity > 0">
-              <p>Brokerage: {{ brokerage | currency }}</p>
-              <p>Total cost: {{ cost | currency }}</p>
-            </div>
-            <div class="flex">
-              <input type="button" class="btn btn-submit" value="Buy">
-              <input type="button" @click="close" class="btn btn-cancel" value="Cancel">
+            <div class="pricing flex-1">
+              <div class="pricing-fields">
+                <p>Brokerage: {{ brokerage | currency }}</p>
+                <p>Total cost: {{ cost | currency }}</p>
+              </div>
+              <div class="pricing-fields">
+                <input type="button" class="btn btn-submit" value="Buy">
+              </div>
             </div>
           </form>
         </div>
@@ -45,21 +42,19 @@ export default {
   data() {
     return {
       showModal: false,
-      quantity: ""
+      quantity: "",
+      brokerage: 0,
+      cost: 0
     };
   },
   props: ["symbol", "name", "price", "timestamp"],
   methods: {
     close() {
       this.$emit("close");
-    }
-  },
-  computed: {
-    brokerage() {
-      return this.price * this.quantity * 0.01 + 50;
     },
-    cost() {
-      return this.price * this.quantity * 1.01 + 50;
+    calcCost() {
+      this.brokerage = this.price * this.quantity * 0.01 + 50;
+      this.cost = this.price * this.quantity * 1.01 + 50;
     }
   },
   filters: {
@@ -81,6 +76,10 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+* {
+  box-sizing: border-box;
+}
+
 .modal-mask {
   position: fixed;
   z-index: 9998;
@@ -91,5 +90,54 @@ export default {
   background-color: rgba(0, 0, 0, 0.5);
   display: table;
   transition: opacity 0.3s ease;
+}
+
+.modal-wrapper {
+  display: table-cell;
+  vertical-align: middle;
+}
+
+.modal-container {
+  width: 480px;
+  margin: 0 auto;
+  border-radius: 2px;
+  background-color: #fff;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
+  transition: all 300ms ease;
+}
+
+.quantity {
+  padding: 20px 30px;
+}
+
+.pricing {
+  background: #fbab39;
+}
+
+.pricing-fields {
+  padding: 16px;
+}
+
+.flex-1 {
+  flex: 1;
+}
+
+.flex-2 {
+  flex: 2;
+}
+
+.btn {
+  color: #fbab39;
+  background: #fff;
+  border: none;
+  border-radius: 0;
+}
+
+label {
+  flex: 1;
+}
+
+input {
+  flex: 1;
 }
 </style>
