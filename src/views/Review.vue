b2d61a8d3b5be88a96b2b95d8e7e94f3b20ef9d6
rmit-s3484880-emily-yorgey <s3484880@student.rmit.edu.au>
2018-04-21 22:21:43 +1000

    initiated child component userTransactions

--- src/views/Review.vue
+++ src/views/Review.vue
@@ -12,73 +12,13 @@
       </div>
       <!-- Transaction history -->
       <div v-if="showHistory">
-        <div class="sharesBought">Shares Bought</div>
-        <table>
-          <thead>
-            <tr>
-              <th class="thead-1">Date
+        <user-transactions :transactions="transactions"></user-transactions>
 
-              </th>
-              <th class="thead-2">Company Name
-
-              </th>
-              <th class="thead-3">Quantity
-
-              </th>
-              <th class="thead-4">Price
-
-              </th>
-              <th class="thead-5">Cost
-
-              </th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr v-for="transaction in transactions" :key="transaction._id">
-              <td>{{transaction.id}}</td>
-              <td>{{transaction.share}}</td>
-              <td>{{transaction.quantity}}</td>
-              <td>{{transaction.price}}</td>
-              <td>{{transaction.quantity * transaction.price | currency }}</td>
-            </tr>
-          </tbody>
-        </table>
-
-        <div class="sharesSold">Shares Sold</div>
-        <table>
-          <thead>
-            <tr>
-              <th class="thead-1">Date
-              </th>
-              <th class="thead-2">Company Name
-              </th>
-              <th class="thead-3">Quantity
-              </th>
-              <th class="thead-4">Price
-              </th>
-              <th class="thead-5">Cost
-              </th>
-            </tr>
-          </thead>
-
-          <tbody>
-            <tr v-for="transaction in transactions" :key="transaction._id">
-              <td>{{transaction.id}}</td>
-              <td>{{transaction.share}}</td>
-              <td>{{transaction.quantity}}</td>
-              <td>{{transaction.price}}</td>
-              <td>{{transaction.quantity * transaction.price | currency }}</td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
-    </div>
-  </div>
 </template>
 
 <script>
 import axios from "axios";
-
+import transactions from "@/components/UserTransactions.vue";
 import NavApp from "@/components/partials/NavApp.vue";
 import UserHoldings from "@/components/UserHoldings.vue";
 
@@ -86,7 +26,8 @@ export default {
   name: "Review",
   components: {
     NavApp,
-    UserHoldings
+    UserHoldings,
+    UserTransactions
   },
   data() {
     return {
@@ -155,47 +96,5 @@ export default {
   color: #fff;
   background: var(--primary-color);
   box-shadow: none;
-}
-
-.sharesBought,
-.sharesSold {
-  width: 100%;
-  max-width: 100%;
-  margin: 2rem 0 0.5rem 0;
-  background-color: #f99f46;
-  font-size: 14px;
-  font-weight: 500;
-  text-transform: uppercase;
-  letter-spacing: 0.5px;
-  border-radius: 4px;
-  padding: 10px;
-}
-
-table {
-  width: 100%;
-  background: #fff;
-  border: 1px solid #d3d3d3;
-  border-radius: 4px;
-}
-
-th {
-  padding: 0.5rem;
-  font-size: 14px;
-  font-weight: 500;
-}
-
-tr:nth-child(even) {
-  background-color: #f2f2f2;
-}
-
-thead {
-  text-align: left;
-  margin-right: 0.5rem;
-}
-
-td {
-  padding: 0.5rem;
-  font-size: 13px;
-  font-weight: 500;
 }
 </style>
