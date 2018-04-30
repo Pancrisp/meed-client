a0be1baa53361f44ab85ea9b9e48af5abc5edec6
Ervin Chua <ervinchua@gmail.com>
2018-04-30 17:49:19 +1000

    Improve trading account creation process
    - Form for creating a trading account now takes in user input for setting the trading account name

---
+++ src/components/cards/NewAccount.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="card">
+    <div class="card-container">
+      <label class="label">Trading account name</label>
+      <input class="namefield" v-model="name" type="text">
+      <div class="btn btn-create" @click="create">Create a trading account</div>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: "NewAccount",
+  data() {
+    return {
+      name: ""
+    };
+  },
+  methods: {
+    create() {
+      axios
+        .post(`https://fierce-lake-99257.herokuapp.com/accounts`, {
+          userId: "5addccb64940710cfb2a0b74"
+        })
+        .then(res => {
+          console.log(res);
+
+          if (res.status == 201) {
+            setTimeout(this.$router.push("/dashboard"), 2000);
+          }
+        });
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.card {
+  width: 560px;
+  margin: 1rem;
+  box-shadow: 0 4px 6px 0 rgba(0, 0, 0, 0.05);
+  transition: 250ms ease;
+  border-radius: 4px;
+  background: #fff;
+  overflow: hidden;
+}
+
+.card-container {
+  margin: 1rem auto;
+  max-width: 400px;
+}
+
+.namefield {
+  margin-top: 0.5rem;
+}
+
+.btn-create {
+  margin: 0 auto;
+  padding: 1rem;
+  background: #e8e8e8;
+  cursor: pointer;
+
+  &:hover {
+    background: darken(#e8e8e8, 5%);
+  }
+}
+</style>
