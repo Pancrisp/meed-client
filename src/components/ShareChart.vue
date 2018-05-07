a0a141c50e3799911672a7673596f3f43d01f9b0
Emily Yorgey <s3484880@student.rmit.edu.au>
2018-05-07 10:47:55 +1000

    highcharts Access chart instance via refs attempt

--- src/components/ShareChart.vue
+++ src/components/ShareChart.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="container flex px3">
     <NavApp/>
-    <div class="workspace">
-
-      <div id ="container">
-
-     </div>
-    </div>
+      <div class="workspace">
+        <div id ="container">
+            <highcharts :options="options"></highcharts>
+            <highstock :options="options"></highstock>
+        </div>
+      </div>
   </div>
 </template>
 
@@ -17,70 +17,70 @@ import Highcharts from "highcharts/highstock";
 
 Vue.use(VueHighcharts, { Highcharts });
 
+var chart = Highcharts.stockChart("container", {
+  //format stockchart
+  legend: {
+    layout: "vertical",
+    align: "right",
+    verticalAlign: "middle",
+    borderWidth: 0
+  },
+
+  //ready() {
+  //(function(shares) {
+  chart: {
+    events: {
+      load: function(shares) {
+        // set up the updating of the chart each second
+        var series = this.series[0];
+        setInterval(function() {
+          var x = new Date().getTime(), // current time
+            y = Math.round(Math.random() * 100);
+          series.addPoint([x, y], true, true);
+        }, 1000);
+      }
+    },
+
+    xAxis: {
+      type: "datetime",
+      visibile: true
+    },
+
+    yAxis: {
+      title: {
+        text: shares.name + "Prices"
+      }
+    },
+
+    plotOptions: {},
+    series: [
+      {
+        name: "shares",
+        data: function() {
+          // generate an array of shares data
+          var shares = [],
+            time = new Date().getTime(),
+            i;
+
+          for (i = -999; i <= 0; i += 1) {
+            data.push([time + i * 1000, Math.round(Math.random() * 100)]);
+          }
+          return shares.price;
+        }
+      }
+    ]
+  }
+});
+
 export default {
   name: "ShareChart",
-  components: {
-    ShareItem,
-    highcharts: Chart
-  },
+  components: {},
   data() {
     return {
-      shares: []
+      stockChart: chart
     };
   },
-  // props: ["shares"],
-  ready() {
-    $(function(data) {
-      var chart = Highcharts.stockChart("container", {
-        rangeSelector: {
-          selected: 4
-        },
-
-        xAxis: {
-          // categories: ["Mon", "Tues", "Wed", "Thurs"],
-          type: "datetime",
-          dateTimeLabelFormats: {
-            day: "%e of %b"
-          },
-          visibile: true
-        },
-
-        yAxis: {
-          title: {
-            text: shares.name + "Prices"
-          }
-        },
-        plotOptions: {},
-        series: [
-          {
-            name: "shares",
-            data: createData()
-            // pointInterval: 24 * 3600 * 1000 // one day
-          }
-        ]
-      });
-    });
-  },
-
-  computed: {
-    function() {
-      startDate = Date.UTC(2018, 5, 1);
-      (createData = function(beginDate) {
-        shares = [];
-        for (var i = 0; i < 11; i++) {
-          shares.push([beginDate + 3600 * 1000 * 24 * i, Math.random() * 100]);
-        }
-        return shares;
-      }),
-        (nextDay = function() {
-          startDate += 3600 * 1000 * 24;
-          chart.series[0].setData(createData(startDate), true);
-          setTimeout(nextDay, 2000);
-        });
-
-      setTimeout(nextDay, 2000);
-    }
-  }
+  props: ["shares"]
 };
 </script>
 
