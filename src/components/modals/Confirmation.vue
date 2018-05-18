4420a2f5063293eac42c3c850fbb4b8101ad6fc8
Emily Yorgey <s3484880@student.rmit.edu.au>
2018-05-18 14:22:33 +1000

    sorted styling and alignment in confirmation modal

--- src/components/modals/Confirmation.vue
+++ src/components/modals/Confirmation.vue
@@ -1,14 +1,19 @@
 <template>
   <modal :show="show" @close="close">
     <div class="blockModal">
+      <br>
       <img src="../../../public/block_icon.svg" alt="blockicon">
+      <br>
       <h3 class="label">Are you sure</h3>
       <p class="label">Are you sure you want to block {{ name }}?</p>
-
-      <div class="confirm-field">
-        <input type="button" class="btn btn-block" value="Block" @click="block">
-        <input type="button" class="btn btn-cancel" value="Cancel" @click="cancel">
-      </div>
+      <br>
+      <table class="confirm-field" align="center">
+        <th>
+          <td><input type="button" class="btn btn-cancel" value="Cancel" @click="cancel"></td>
+          <td></td>
+          <td><input type="button" class="btn btn-block" value="Block" @click="block"></td>
+        </th>
+      </table>
     </div>
   </modal>
 </template>
@@ -39,15 +44,31 @@ export default {
   color: #fff;
   background: rgb(231, 43, 43);
   transition: all 300ms ease;
+  width: 120px;
 }
 
 .btn-cancel {
   color: #fff;
   background: #838282;
   transition: all 300ms ease;
+  width: 120px;
 }
 
 .blockModal img {
-  width: 240px;
+  width: 130px;
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+}
+
+td {
+  padding: 0.5rem 1rem;
+  font-weight: 500;
+  width: 10px;
+}
+
+.label,
+.blockModal img {
+  text-align: center;
 }
 </style>
